f96093d0a7a1181ded8f05a83f0d6d5abc5cf738
Yohanes <yohanes,p1415@gmail.com>
2024-06-26 20:26:00 +0900

    File

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+// Hamburger Menu Toggle
+const navbarNav = document.querySelector(".navbar-nav");
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik diluar Sidebar
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
